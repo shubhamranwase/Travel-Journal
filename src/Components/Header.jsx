64a243beef0d4a8309c6bbc3070817b880c18670
Shubham Ranwase <shubhamranwase961@gmail.com>
2026-02-27 11:21:02 +0530

    dark mode added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons'
+import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Header() {
+    const ToggleDarkMode = () => {
+        document.body.classList.toggle("dark");
+    }
     return(
         <nav className="navbar">
-            <div className="globe"><FontAwesomeIcon icon={faEarthAmericas} /></div>
-            <span>My Travel Journal</span>
+            <div className="main-Header">
+                <div className="globe"><FontAwesomeIcon icon={faEarthAmericas} /></div>
+                <span>My Travel Journal</span>
+            </div>
+            <div className="mode" onClick={ToggleDarkMode} ><FontAwesomeIcon icon={faCircleHalfStroke} /></div>
         </nav>
     )
 }
