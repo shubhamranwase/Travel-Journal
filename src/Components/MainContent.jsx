1ddce45689e56d8b048eeab9be57ec05dc51227c
Shubham Ranwase <shubhamranwase961@gmail.com>
2026-03-13 16:08:56 +0530

    fixed css container placements

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -9,7 +9,9 @@ export default function MainContent(props) {
         return (
     <div className="main-container">
     <div className="card" key={index}>
-    <img className="img-container"  src={card.image} alt="img" />
+    <div className="img-main-container">
+            <img className="img-container"  src={card.image} alt="img" />
+    </div>
     <div className="text-container">
         <div className="info-container">
         <a className='country' href={card.countrylink}><FontAwesomeIcon icon={faLocationDot} />{card.country}</a>
